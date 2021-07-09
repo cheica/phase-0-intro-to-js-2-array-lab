3baf6a8708776e14debc6e796568d74b084de6f3
Chelsea <chelsea.icaza10@hotmail.com>
2021-07-09 14:39:46 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+   const allCats = [...cats, name];
+   return allCats;
+}
+
+function prependCat(name){
+    const allCatz = [name, ...cats];
+    return allCatz;
+}
+
+function removeLastCat(){
+   return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
